98f324e5e1df9f870ada56dbd64af75fb4c98eb1
juniormesquitadandao <juniormesquitadandao@gmail.com>
2022-07-04 01:04:30 +0000

    cd

--- devops/codebuild/build.sh
+++ devops/codebuild/build.sh
@@ -8,4 +8,5 @@ docker build . \
   --build-arg ARG_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   --build-arg ARG_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   --build-arg ARG_RAILS_MASTER_KEY=$RAILS_MASTER_KEY \
+  --build-arg ARG_DATABASE_URL=$DATABASE_URL \
   -q
