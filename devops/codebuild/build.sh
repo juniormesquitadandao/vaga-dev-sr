9ac572b1ebeaf9a1dea68215c09826550c4d555e
juniormesquitadandao <juniormesquitadandao@gmail.com>
2022-07-03 21:11:19 -0300

    cd

--- devops/codebuild/build.sh
+++ devops/codebuild/build.sh
@@ -2,5 +2,10 @@
 
 set -e
 
-docker build . -f devops/codebuild/Dockerfile -t vaga-dev-sr --build-arg ARG_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID  --build-arg ARG_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -q
-
+docker build . \
+  -f devops/codebuild/Dockerfile \
+  -t vaga-dev-sr \
+  --build-arg ARG_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  --build-arg ARG_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  --build-arg ARG_RAILS_MASTER_KEY=$RAILS_MASTER_KEY \
+  -q
